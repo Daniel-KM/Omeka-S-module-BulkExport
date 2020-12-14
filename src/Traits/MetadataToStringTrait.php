b17fce9d4251c33f2225f3ac4687b29ec439eeb2
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-12-14 00:00:00 +0000

    Fixed export of media urls.

--- src/Traits/MetadataToStringTrait.php
+++ src/Traits/MetadataToStringTrait.php
@@ -93,7 +93,7 @@ trait MetadataToStringTrait
                         }
                     }
                 } elseif ($resource->resourceName() === 'media') {
-                    $originalUrl = $media->originalUrl();
+                    $originalUrl = $resource->originalUrl();
                     if ($originalUrl) {
                         $result[] = $originalUrl;
                     }
