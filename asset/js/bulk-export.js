db0060224ab69e922633e5e679ef28aa95ceae48
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-07-01 00:00:00 +0000

    Added aria attributes on details summary.

---
+++ asset/js/bulk-export.js
@@ -0,0 +1,9 @@
+(function() {
+    $(document).ready(function() {
+
+        $(document).on('click', 'summary.bulk-export-summary', function() {
+            this.setAttribute('aria-expanded', this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
+        });
+
+    });
+})();
